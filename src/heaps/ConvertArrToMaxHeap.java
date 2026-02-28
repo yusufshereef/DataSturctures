efb3ace9cb349cffdaf315f79370c906d9a50cd9
yusuf shereef <yusufshereef69@gmail.com>
2026-02-28 16:01:07 +0530

    added heap-sort in ConvertArrToMaxHeap

--- src/heaps/ConvertArrToMaxHeap.java
+++ src/heaps/ConvertArrToMaxHeap.java
@@ -7,7 +7,7 @@ public class ConvertArrToMaxHeap {
         }
     }
 
-    //adjust ith node
+    //adjust a parent node ( time-complexity is O(logn) )
     private void adjust(int[] a, int i, int size){
         int key = a[i];
         int j = 2 * i;
@@ -24,4 +24,23 @@ public class ConvertArrToMaxHeap {
         a[j/2] = key;
     }
 
+    public void heapSortDescending(int[] a, int size){
+        //construct initial max heap
+        heapify(a, size);
+        //swap root node (max ele) with last leaf node and adjust wrt to root node
+        // doing this for ntimes makes the array sorted in descending order
+        int n = a.length-1;
+        for(int i=0; i<n; i++){
+            //swap root node with last node
+            int temp = a[1];
+            a[1] = a[size];
+            a[size] = temp;
+
+            //now decrease the size to exclude the swapped node
+            size--;
+
+            //call adjust wrt to the root node as it is the only node which was disturbed
+            adjust(a, 1, size);
+        }
+    }
 }
